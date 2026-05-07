a12d7c1baa8908392cc333944291256c28a4f6da
albertrodriguezd <albert.rodriguez.domingo@estudiantat.upc.edu>
2026-05-07 17:18:58 +0200

    nivel5

---
+++ Assets/Scripts/fuego5.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class fuego5 : MonoBehaviour
+{
+    public ParticleSystem Fuego;
+    public objectManager objectManager;
+    public GameObject hojas;
+    public GameObject llave;
+
+    private bool estadoFuego = false;
+    private bool cercaBonsai = false;
+
+    void Start()
+    {
+        Fuego.Stop();
+        Fuego.Clear();
+        llave.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && cercaBonsai)
+        {
+            // ENCENDER
+            if (!estadoFuego)
+            {
+                if (objectManager.cerilla)
+                {
+                    Fuego.Play();
+                    estadoFuego = true;
+                    llave.SetActive(true);
+
+                    // Oculta las hojas
+                    hojas.SetActive(false);
+
+                    Debug.Log("El bonsái se está quemando");
+                }
+                else
+                {
+                    Debug.Log("Necesitas una cerilla");
+                }
+            }
+
+            // APAGAR
+            else
+            {
+                if (objectManager.extintor)
+                {
+                    Fuego.Stop(false, ParticleSystemStopBehavior.StopEmitting);
+
+                    estadoFuego = false;
+
+                    Debug.Log("Fuego apagado");
+                }
+                else
+                {
+                    Debug.Log("Necesitas un extintor");
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cercaBonsai = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cercaBonsai = false;
+        }
+    }
+}
